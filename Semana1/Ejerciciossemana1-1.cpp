82ecd910591c4b22e5e5d7764ed16face88072c4
Moepiii <rafa.valera98@gmail.com>
2024-05-08 17:31:39 -0400

    actualizacion

---
+++ Semana1/Ejerciciossemana1-1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    /*
+    El problema inicialmente solicita 3 cosas:Debemos decirle el numero de casos
+    Tambien debemos decirle cuantas cartas seran usadas y cuantas son las que puedo juntar para
+    eliminar y hacer el cambio por otras en caso de ser similares. 
+    */
+
+    int x;
+    cin>>x;
+
+    while(x--){
+      
+       int n,k;
+       cin>>n>>k;
+
+     /*
+       Ahora toca lo complejo.
+       Necesitamos algo que nos permita guardar los valores que van a tener las cartas que 
+       es proporcional al valor que coloquemos en n, esto lo lograremos con un vector que
+       vamos a enlazar con el valor de n.Tambien necesitamos implementar el proceso de guardado
+       esto se logra con un for.
+     */
+      
+      vector<int> valorcartas(n);
+
+      for (int i = 0; i < n; i++){
+        cin >> valorcartas[i];
+      }
+       
+      /*
+       vamos a definir una constante de tipo entero que no va a variar
+       esto para posteriormente crear un vector con un tamañó maximo
+       en principio este vector va a guardar cuantas veces se repite un numero
+       por medio de un for que va viendo si los valores son iguales o no.
+     */
+
+      const int maximo = 800; 
+      vector<int> repetidos(maximo, 0);
+
+      for (int veces : valorcartas){
+       repetidos[veces]++;
+      }        
+      
+
+      /*
+      Finalmente lograremos la resolucion por medio de un ultimo for
+      para esto declaramos un int contador que se le sumara 1 cada vez
+      que la condicion del if se cumpla, que basicamente si el vector 
+      repetidos en la posicion 1 es >= k sumarle uno a contador
+      aqui es donde puede que sea deficiente porque este for se ejecuta hasta
+      que alcance el valor de maximo que es colocado por nosotros.
+      */
+    
+       int contador = 0;
+       for (int i = 0; i <maximo; i++) {
+         if (repetidos[i] >= k) {
+            contador++;
+          }
+        }
+
+      /*
+      En el resultado pueden pasar dos cosas que sea directamente el valos de n o
+      ser k-1 entonces es necesario el if para casos.
+      */
+        
+       int respuesta;
+       if (contador > 0) {
+       respuesta = k - 1;
+       } else {
+       respuesta = n;
+       }
+       cout << respuesta << endl;
+       
+    }
+
+    return 0;
+}
+
